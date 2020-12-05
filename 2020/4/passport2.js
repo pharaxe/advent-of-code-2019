295a76d566a89444d5e1b3d18f97d47ea0f6847c
Ben Sweedler <bsweedler@gmail.com>
2020-12-04 21:01:51 -0600

    that one was harder!

--- 2020/4/passport2.js
+++ 2020/4/passport2.js
@@ -3,22 +3,25 @@
 import * as lineReader from "line-reader";
 
 const main = (lines) => {
-  const passportStrings = lines.reduce((accum, cur) => {
-    // top is the current passport we are parsing.
-    const top = accum.length - 1;
+  const passportStrings = lines.reduce(
+    (accum, cur) => {
+      // top is the current passport we are parsing.
+      const top = accum.length - 1;
 
-    if (cur.trim() === "") {
-      // done parsing the top passport,
-      // put an empty passport string on the stack.
-      return [...accum, ""];
-    }
-    {
-      // we found a line of passport fields
-      // to add to the top passport string.
-      accum[top] += cur + " ";
-      return accum;
-    }
-  }, [""]);
+      if (cur.trim() === "") {
+        // done parsing the top passport,
+        // put an empty passport string on the stack.
+        return [...accum, ""];
+      }
+      {
+        // we found a line of passport fields
+        // to add to the top passport string.
+        accum[top] += cur + " ";
+        return accum;
+      }
+    },
+    [""]
+  );
 
   const passports = passportStrings.map((passportString) =>
     passportString.trim().split(" ")
@@ -28,13 +31,51 @@ const main = (lines) => {
     passport.filter((passportField) => !passportField.startsWith("cid"))
   );
 
-  let validPassports  = 0;
-  passportsWithoutCid.forEach(passport => {
-    if (passport.length === 7) {
-      validPassports++;
-    }
+  const passportObjects = passportsWithoutCid.map((passport) => {
+    const keyValueArrays = passport.map((fieldString) => {
+      return fieldString.split(":");
+    });
+    const chonkyPassportObject = {};
+    keyValueArrays.forEach((pair) => {
+      const [key, value] = pair;
+      chonkyPassportObject[key] = value;
+    });
+    return chonkyPassportObject;
   });
-  console.log(validPassports);
+
+  const passportsWithRequiredFields = passportObjects.filter((passport) => {
+    return Object.keys(passport).length === 7;
+  });
+
+  const passportFieldValidty = passportsWithRequiredFields.map((passport) => {
+    return Object.keys(passport).map((field) => {
+      return validationSchema[field](passport[field]); // magic happens here
+    });
+  });
+
+  const passportTotalValidty = passportFieldValidty.map((validityArray) => {
+    return validityArray.every(Boolean);
+  });
+
+  const onlyValidPassports = passportTotalValidty.filter(Boolean);
+
+  console.log(`The number of valid passports is ${onlyValidPassports.length}`);
+};
+
+const validationSchema = {
+  byr: (value) =>
+    value.length === 4 && Number(value) >= 1920 && Number(value) <= 2002,
+  iyr: (value) =>
+    value.length === 4 && Number(value) >= 2010 && Number(value) <= 2020,
+  eyr: (value) =>
+    value.length === 4 && Number(value) >= 2020 && Number(value) <= 2030,
+  hgt: (value) =>
+    (value.endsWith("in") && parseInt(value) >= 59 && parseInt(value) <= 76) ||
+    (value.endsWith("cm") && parseInt(value) >= 150 && parseInt(value) <= 193),
+  hcl: (value) => /#[0-9a-f]{6}/.test(value),
+  ecl: (value) =>
+    ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(value),
+  pid: (value) => value.length === 9 && !isNaN(parseInt(value)),
 };
 
 const filename = process.argv[2];
