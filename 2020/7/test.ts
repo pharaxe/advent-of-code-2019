881466b968b4f0c633905678d44889002ddbd376
Ben Sweedler <bsweedler@gmail.com>
2020-12-07 19:51:00 -0600

    refactoring so typescript works

---
+++ 2020/7/test.ts
@@ -0,0 +1,12 @@
+import * as lineReader from "line-reader";
+
+const main = (lines: string[]) => {};
+
+var filename = process.argv[2];
+var lines = [];
+lineReader.eachLine(filename, function (line, last) {
+  lines.push(line);
+  if (last) {
+    main(lines);
+  }
+});
