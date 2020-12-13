ca8847a888f01d6d9ebc7d366ac7587e427ba199
Ben Sweedler <bsweedler@gmail.com>
2020-12-13 14:41:05 -0600

    day 13 part one completed

--- 2020/13/bus.ts
+++ 2020/13/bus.ts
@@ -1,8 +1,27 @@
 import * as lineReader from "line-reader";
 
+type Bus = {
+  id: number;
+  timeWaiting: number;
+};
+
 const main = (lines: string[]): void => {
-  console.log(lines);
-}
+  const earliest: number = parseInt(lines[0]);
+  const buses: Bus[] = lines[1]
+    .split(",")
+    .filter((busId) => busId !== "x")
+    .map((busId) => parseInt(busId))
+    .map((id) => ({
+      id,
+      timeWaiting: id - (earliest % id),
+    }));
+
+  const leastWaiting: Bus = buses.reduce((least, cur) => {
+    return cur.timeWaiting < least.timeWaiting ? cur : least;
+  }, buses[0]);
+
+  console.log(leastWaiting.timeWaiting * leastWaiting.id);
+};
 
 var filename = process.argv[2];
 var lines = [];
